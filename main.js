7cfa466f05e3db99b4575bfe061ba16efb7fdc55
Christopher Hill <thorsdaddy77@gmail.com>
2016-05-21 14:51:06 -0500

    javascript sonnet exercise

---
+++ main.js
@@ -0,0 +1,61 @@
+// // How many hours are in this year
+// var yearHours = (365 * 24 * 60);
+// var yearMinutes = yearHours / (60);
+// // console.log("one year in hours", yearMinutes);
+// console.log("yearHours");
+// console.log("yearMinutes");
+// // How many minutes are in a decade without leap years
+// // console.log("minutes/decade", decadeMinutes);
+// var phrase = "hello there"
+// var newPhrase = phrase.replace(/l/g, "t")
+// // How many seconds old I am
+// // console.log("my age in seconds", ageSeconds);
+
+// // If I am older than some arbitrary number, log "I'm old", else "I'm young"
+
+// var oneSecond = 1000;
+// var oneMinute = oneSecond * 60;
+// var oneHour = oneMinute * 60;
+// var oneDay = oneHour * 24;
+// var oneYear = oneDay * 365;
+// // study this crap below!!!
+// // var phraseString = document.getElementById("myPhrase").innerHTML;
+
+// ############## for loops #####################
+// Write a for loop that increments the counter variable by 10 each time, and displays the value.
+
+// Write a for loop that divides the counter variable by 2 each time, and displays the value.
+
+// Augment the loop to insert each new value into an array
+
+// Write a loop that starts at 100 and decrements a variable by 1. If the number is even, add the number to the beginning of an array, else add it to the end of the array.
+
+
+
+// for (var i = 0; i <= 100; i+=10) {
+// 	console.log("count by ten", i);
+// }
+
+
+// for (var i = 10; i > 2 ; i/=2) {
+// 	console.log(i);
+// }
+
+
+// var myArray = [];
+// for (var i = 100; i > 1; i/=2) {
+// 	myArray.push(i);
+// }
+// console.log(myArray);
+
+
+// var evens = [];
+// for (var i = 0; i > 0; i--) {
+// 	if (i % 2 === 0);
+// }
+// {
+// 	evens.unshift(i);
+// }
+// else {
+// 	evens.push(i);
+// }
---
+++ sonnet.js
@@ -0,0 +1,18 @@
+
+
+
+var poem = document.getElementById("sonnet").innerHTML;
+    document.write(sonnet);
+
+var chilly = poem.replace("winter", "yuletide");
+
+var n = chilly.replace(/the/g, "a large");
+
+var element = document.getElementById("sonnet");
+element.innerHTML = n;
+
+var str = poem.indexOf("orphans");
+
+var num = poem.length;
+
+
